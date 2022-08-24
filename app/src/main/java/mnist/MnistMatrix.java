46d58b5ac188676c6e18b286fc2f1b733a935014
Eugene Matsumura <eugene.bb1@gmail.com>
2022-08-24 21:43:18 +0200

    feat: :sparkles: add easy data display

--- app/src/main/java/mnist/MnistMatrix.java
+++ app/src/main/java/mnist/MnistMatrix.java
@@ -27,4 +27,37 @@ public class MnistMatrix {
   public int getNumberOfRows() { return nRows; }
 
   public int getNumberOfColumns() { return nCols; }
+
+  public String toString() {
+    String result = "";
+    for (int r = 0; r < nRows; r++) {
+      for (int c = 0; c < nCols; c++) {
+        result += data[r][c] + " ";
+      }
+      result += "\n";
+    }
+    return result;
+  }
+
+  public String toASCII() {
+    String result = "";
+    for (int r = 0; r < nRows; r++) {
+      for (int c = 0; c < nCols; c++) {
+        result += data[r][c] == 0 ? " " : "X";
+      }
+      result += "\n";
+    }
+    return result;
+  }
+
+  public static String toASCII(float[] data) {
+    String result = "";
+    for (int r = 0; r < 28; r++) {
+      for (int c = 0; c < 28; c++) {
+        result += data[r * 28 + c] == 0 ? " " : "X";
+      }
+      result += "\n";
+    }
+    return result;
+  }
 }
